2084b45f77596a08eaed1fb1eac2108bdfa16b45
chenttt2001 <112816005+chenttt2001@users.noreply.github.com>
2022-10-10 22:33:23 +0800

    Complete the registry

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ MODEL = register.Registry('models')
 class net():
     pass
 
+@MODEL.register_module()
+class net2():
+    pass
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
