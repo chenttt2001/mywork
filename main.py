808db1d4f3df238427eeaf931912052e8e06d75a
chenttt2001 <112816005+chenttt2001@users.noreply.github.com>
2022-10-10 22:24:05 +0800

    Complete the registry

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import register
+
+MODEL = register.Registry('models')
+
+
+@MODEL.register_module()
+class net():
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    A=MODEL.build(dict(type='net'))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
